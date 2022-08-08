c43a23674a954f2d7bcbe99ce4a1473ef6dc7309
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-08 11:42:48 -0700

    add render for contact card

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ContactCard from './ContactCard';
 
-const ContactList = () => {
+const ContactList = (props) => {
+
+  const renderContactList = props.contacts.map((contact) => {
+    return(
+      <ContactCard contact={contact}></ContactCard>
+    );
+  });
+
   return (
     <div className="ui celled list">
-      Contact List
+      Contact List:
+      {renderContactList}
     </div>
   );
 };
